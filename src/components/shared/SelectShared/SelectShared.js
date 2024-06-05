9439a6d181ec4feba9dfd0e2a15c740aec4912d7
Mykola Fedotov <fedotovns@gmail.com>
2024-06-05 21:53:50 +0200

    removed comments

--- src/components/shared/SelectShared/SelectShared.js
+++ src/components/shared/SelectShared/SelectShared.js
@@ -1,5 +1,5 @@
 import Select from "react-select";
-import customStyles from "./customStyles"; // Імпорт стилів
+import customStyles from "./customStyles";
 
 const SelectShared = ({ field, options, placeholder }) => {
   return <Select {...field} options={options} placeholder={placeholder} styles={customStyles} />;
