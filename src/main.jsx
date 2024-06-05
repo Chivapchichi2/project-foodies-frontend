f772a00fd724d8fa1b55eca39cb4d6ce48d5e830
Podmaskov <podmaskov@gmail.com>
2024-06-05 18:45:14 +0200

    connected persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
 import { App } from "./App.jsx";
-import { store } from "src/store/store";
+import { store, persistor } from "src/store/store";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
