24daa293f325466ae91c124675afe438bf1102b8
Mykola Fedotov <fedotovns@gmail.com>
2024-06-05 22:22:44 +0200

    added font styles fro dropdown menu

--- src/components/shared/SelectShared/customStyles.js
+++ src/components/shared/SelectShared/customStyles.js
@@ -1,16 +1,20 @@
 const customStyles = {
-  control: styles => ({
+  control: (styles) => ({
     ...styles,
-    padding: '14px',
-    borderRadius: '30px',
-    border: '1px solid rgba(5, 5, 5, 0.12)',
+    padding: "14px",
+    borderRadius: "30px",
+    border: "1px solid rgba(5, 5, 5, 0.12)",
   }),
-  menu: styles => ({
+  menu: (styles) => ({
     ...styles,
-    padding: '16px 18px',
-    borderRadius: '15px',
-    border: '1px solid rgba(5, 5, 5, 0.20)',
+    padding: "16px 18px",
+    borderRadius: "15px",
+    border: "1px solid rgba(5, 5, 5, 0.20)",
+    fontSize: "16px",
+    fontWeight: "500",
+    lineHeight: "calc(24 / 16)",
+    letterSpacing: "-0.32px",
   }),
 };
-
+//ed
 export default customStyles;
