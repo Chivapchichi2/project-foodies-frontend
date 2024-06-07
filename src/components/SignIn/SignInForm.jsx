e12576e0a2b55b6cdc90b15fcf0bbda9b1a83354
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2024-06-07 11:19:36 +0300

    fix: lint

--- src/components/SignIn/SignInForm.jsx
+++ src/components/SignIn/SignInForm.jsx
@@ -8,6 +8,8 @@ import {Input} from "../shared/Input/Input.jsx";
 import {sinInSchema} from "./SignInSchema.js";
 import Button from "../shared/Button/Button.jsx";
 import ModalTitle from "../shared/ModalTitle/ModalTitle.jsx";
+import {CustomModal} from "../shared/CustomModal/CustomModal.jsx";
+import {SignUpForm} from "../SignUp/SignUpForm.jsx";
 
 
 export const SignInForm = () => {
@@ -46,37 +48,42 @@ export const SignInForm = () => {
     };
 
     return (
-        <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
-            <ModalTitle text={'SIGN IN'} />
-            <ul className={styles.list}>
-                <li className={styles.item}>
-                    <Input
-                        placeholder={'Email*'}
-                        type={'email'}
-                        register={register}
-                        name="email"
-                        hasText={valueInput.email?.length > 0}
-                    />
-                    {errors.email && <span className={styles.error}>{errors.email.message}</span>}
-                </li>
-                <li className={styles.item}>
-                    <Input
-                        placeholder={'Password'}
-                        type={showPassword ? 'text' : 'password'}
-                        iconId={showPassword ? "icon-eye" : "icon-eye-off"}
-                        togglePasswordVisibility={togglePasswordVisibility}
-                        register={register}
-                        name="password"
-                        hasText={valueInput.password?.length > 0}
-                    />
-                    {errors.password && <span className={styles.error}>{errors.password.message}</span>}
-                </li>
-            </ul>
-            <Button type="submit"  text="SIGN IN" variant={'auth'} />
-            <p className={styles.text}>
-                Don't have an account? <button className={styles.link} onClick={handleClickSingUp}>Create an account</button>
-            </p>
-        </form>
+<>
+    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+        <ModalTitle text={'SIGN IN'} />
+        <ul className={styles.list}>
+            <li className={styles.item}>
+                <Input
+                    placeholder={'Email*'}
+                    type={'email'}
+                    register={register}
+                    name="email"
+                    hasText={valueInput.email?.length > 0}
+                />
+                {errors.email && <span className={styles.error}>{errors.email.message}</span>}
+            </li>
+            <li className={styles.item}>
+                <Input
+                    placeholder={'Password'}
+                    type={showPassword ? 'text' : 'password'}
+                    iconId={showPassword ? "icon-eye" : "icon-eye-off"}
+                    togglePasswordVisibility={togglePasswordVisibility}
+                    register={register}
+                    name="password"
+                    hasText={valueInput.password?.length > 0}
+                />
+                {errors.password && <span className={styles.error}>{errors.password.message}</span>}
+            </li>
+        </ul>
+        <Button type="submit"  text="SIGN IN" variant={'auth'} />
+        <p className={styles.text}>
+            {"Don't have an account?"} <button className={styles.link} onClick={handleClickSingUp}>Create an account</button>
+        </p>
+    </form>
+    {openSingUp && (<CustomModal isOpen={openSingUp} onClose={() => setOpenSingUp(false)}>
+        <SignUpForm/>
+    </CustomModal>)}
+</>
     );
 
 }
