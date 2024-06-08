e6bffd9c4170277e8e7a7f91c13ba75cb192f976
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-06-08 19:36:56 +0200

    add static component without logic

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import { ProfileBigCard } from "../ProfileBigCard/ProfileBigCard";
 
 const Layout = () => {
   return (
     <>
-      <Suspense>
+      <ProfileBigCard />
+      {/* <Suspense>
         <Outlet />
-      </Suspense>
+      </Suspense> */}
     </>
   );
 };
