f490f53698fcfc07886a729652c962ce5cfcdf49
Mykola Fedotov <113369815+FedotovMS@users.noreply.github.com>
2024-06-11 18:31:06 +0200

    Add recipe fedotov (#28)
    * created addRecipe component
    
    * fixed formData-image-undefined  and formData-instructions-undefined
    
    * switched to global Title component
    
    * some changes with icons
    
    * added styles uploadbox
    
    * added some styles
    
    * made ingredient card
    
    * added some styles
    
    * changed control method for input in ImageUploader
    
    * added some styles, modified Input - added classname prop
    
    * modified Input
    
    * styles to form
    
    * added some styles
    
    * added more style
    
    * styles to buttons
    
    * styles to input
    
    * styles for add ingredient btn
    
    * added some styles

---
+++ src/components/AddRecipeForm/ImageUploader/ImageUploader.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import Icon from "../../shared/Icon/Icon";
+import { Input } from "../../shared/Input/Input";
+import styles from "./ImageUploader.module.css";
+
+const ImageUploader = ({ register, setValue, imagePreview, setImagePreview, errors, watch }) => {
+  useEffect(() => {
+    const subscription = watch((value) => {
+      if (value.image[0]) {
+        setImagePreview(URL.createObjectURL(value.image[0])) && setValue("image", value.image[0]);
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [watch, setImagePreview, setValue]);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.uploadBox}>
+        <label className={styles.customUploadBtn}>
+          <Input type="file" name="image" register={register} setValue={setValue} errors={errors} />
+          {errors.image && <p>{errors.image.message}</p>}
+          {imagePreview && (
+            <img src={imagePreview} alt="Recipe Preview" className={styles.imagePreview} />
+          )}
+          {!imagePreview ? (
+            <div className={styles.btnWrapper}>
+              <Icon
+                iconId="icon-capture-photo-camera"
+                width="50px"
+                height="50"
+                customStyle={styles.photoIcon}
+              />
+              <span>Upload a photo</span>
+            </div>
+          ) : (
+            <span className={styles.uplodAnotherSpan}>Upload another photo</span>
+          )}
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default ImageUploader;
---
+++ src/components/shared/SelectShared/SelectShared.jsx
@@ -0,0 +1,20 @@
+import Select from "react-select";
+import React from "react";
+import customStyles from "./customStyles";
+
+const SelectShared = React.forwardRef(
+  ({ options, placeholder, className, onChange, value }, ref) => (
+    <Select
+      ref={ref}
+      options={options}
+      placeholder={placeholder}
+      className={className}
+      onChange={onChange}
+      value={value}
+      styles={customStyles}
+    />
+  )
+);
+
+SelectShared.displayName = "SelectShared";
+export default SelectShared;
