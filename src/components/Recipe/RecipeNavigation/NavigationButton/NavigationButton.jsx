560654ecbf9ac599827f3a429754f29577f065b1
Artem <podmaskov@gmail.com>
2024-06-11 07:25:33 +0200

    added retina support and fixed the icon issue (#25)

--- src/components/Recipe/RecipeNavigation/NavigationButton/NavigationButton.jsx
+++ src/components/Recipe/RecipeNavigation/NavigationButton/NavigationButton.jsx
@@ -4,19 +4,27 @@ import cx from "classnames";
 
 import styles from "./styles.module.css";
 
-export const NavigationButton = ({ category, url, imgUrl }) => {
+export const NavigationButton = ({ category, url, imgUrl, imgUrl2x }) => {
+  const isRetina = window.devicePixelRatio > 1;
+  const backgroundImage = isRetina && imgUrl2x ? imgUrl2x : imgUrl;
+
   return (
     <NavLink
       className={cx(styles["recept-nav-btn"])}
       to={url}
       style={{
-        backgroundImage: `url(${imgUrl})`,
+        backgroundImage: `url(${backgroundImage})`,
       }}
     >
       <div className={cx(styles["labels-wrap"])}>
         <p className={cx(styles["category"])}>{category}</p>
         <div className={cx(styles["icon-wrap"])}>
-          <Icon style={cx(styles["icon"])} iconId={"icon-arrow-up-right"} width={20} height={20} />
+          <Icon
+            customStyle={cx(styles["icon"])}
+            iconId={"icon-arrow-up-right"}
+            width={20}
+            height={20}
+          />
         </div>
       </div>
     </NavLink>
