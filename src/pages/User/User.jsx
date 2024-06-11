e3efa73294f64b20598423d54a7cec4de62dd91e
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-06-11 20:32:23 +0200

    build error is fixed

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useFetchUserProfileQuery } from "../../store/services/profileService";
