08e6e998ca41462dbf1b14e9f5e8f8d559afb8f2
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2024-06-12 03:15:24 +0300

    fix: header and hero

--- src/components/shared/AuthToggle/AuthToggle.jsx
+++ src/components/shared/AuthToggle/AuthToggle.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-
+import cx from "classnames";
 import styles from "./AuthToggle.module.css";
 
-import {CustomModal} from "../index.js";
-import {SignInForm, SignUpForm} from "src/components";
+import { Button, CustomModal } from "../index.js";
+import { SignInForm, SignUpForm } from "src/components";
 
 const AuthToggle = () => {
   const [active, setActive] = useState("signIn");
@@ -20,43 +20,39 @@ const AuthToggle = () => {
     setModalSignInOpen(true);
   };
 
-  const handleCloseSignUp = () =>  setModalSignUpOpen(false);
+  const handleCloseSignUp = () => setModalSignUpOpen(false);
   const handleCloseSignIn = () => setModalSignInOpen(false);
 
   return (
-      <>
-        <div
-            className={`${styles.toggleContainer} ${
-                styles[active === "signIn" ? "signInActive" : "signUpActive"]
-            }`}
-        >
-          <div className={styles.slider}></div>
-          <button
-              className={`${styles.toggleButton} ${styles.signIn}`}
-              onClick={handleClickSignIn}
-          >
-            SIGN IN
-          </button>
-          <button
-              className={`${styles.toggleButton} ${styles.signUp}`}
-              onClick={handleClickSignUp}
-          >
-            SIGN UP
-          </button>
-        </div>
-        {active === "signIn"
-            ? (
-                <CustomModal isOpen={modalSignInOpen} onClose={handleCloseSignIn}>
-                  <SignInForm handleClickSignUp={handleClickSignUp}/>
-                </CustomModal>
-            )
-            : (
-                <CustomModal isOpen={modalSignUpOpen} onClose={handleCloseSignUp}>
-                  <SignUpForm handleClickSignIn={handleClickSignIn}/>
-                </CustomModal>
-            )
-        }
-      </>
+    <>
+      <div
+        className={`${styles.toggleContainer} ${
+          styles[active === "signIn" ? "signInActive" : "signUpActive"]
+        }`}
+      >
+        <div className={styles.slider}></div>
+        <Button
+          text="SIGN IN"
+          classname={cx(styles.toggleButton, styles.signIn)}
+          onClick={handleClickSignIn}
+        />
+
+        <Button
+          text="SIGN UP"
+          classname={cx(styles.toggleButton, styles.signUp)}
+          onClick={handleClickSignUp}
+        />
+      </div>
+      {active === "signIn" ? (
+        <CustomModal isOpen={modalSignInOpen} onClose={handleCloseSignIn}>
+          <SignInForm handleClickSignUp={handleClickSignUp} />
+        </CustomModal>
+      ) : (
+        <CustomModal isOpen={modalSignUpOpen} onClose={handleCloseSignUp}>
+          <SignUpForm handleClickSignIn={handleClickSignIn} />
+        </CustomModal>
+      )}
+    </>
   );
 };
 
