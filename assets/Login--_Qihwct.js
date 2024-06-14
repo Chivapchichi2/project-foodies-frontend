dc32c1420edefb348464cbd143e81deb138134f4
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2024-06-14 19:30:01 +0300

    Updates

---
+++ assets/Login--_Qihwct.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Dp6vxA_F.js";const t=()=>o.jsx("div",{children:"Login"});export{t as default};
--- assets/SectionTitle-BtJ4igeS.js
+++
@@ -1,1 +0,0 @@
-import{j as e,o as i}from"./index-BHGE9Ffj.js";const o="_section_title_5g8wu_1",c={section_title:o},l=({text:t,addStyle:s=""})=>e.jsx("h2",{className:i(c.section_title,s),children:t});export{l as S};
