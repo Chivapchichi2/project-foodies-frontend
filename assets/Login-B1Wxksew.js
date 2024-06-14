bc605fa33bf34554b2174f0e4b120fd042a5e77c
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2024-06-14 09:43:54 +0300

    Updates

--- assets/Login-B1Wxksew.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CR1s_VMy.js";const t=()=>o.jsx("div",{children:"Login"});export{t as default};
---
+++ assets/Login-DiXH59oT.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BHGE9Ffj.js";const t=()=>o.jsx("div",{children:"Login"});export{t as default};
