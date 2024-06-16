1f0e597b0bc5e79e3ef207706c423fc158baf494
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-06-16 21:23:23 +0200

    Updates

---
+++ assets/SectionTitle-C7wQrdzk.js
@@ -0,0 +1,1 @@
+import{j as e,i}from"./index-D1B0M72U.js";const o="_section_title_rgpxg_1",c={section_title:o},l=({text:t,addStyle:s=""})=>e.jsx("h2",{className:i(c.section_title,s),children:t});export{l as S};
