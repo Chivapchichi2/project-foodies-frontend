d586ffb25e4ace61d2d6b6a564ec2d6301443447
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2024-06-16 15:43:37 +0300

    Updates

--- assets/SectionTitle-B4iEwy4g.js
+++
@@ -1,1 +0,0 @@
-import{j as e,i}from"./index-ClPNBpSU.js";const o="_section_title_rgpxg_1",c={section_title:o},l=({text:t,addStyle:s=""})=>e.jsx("h2",{className:i(c.section_title,s),children:t});export{l as S};
