40c45e700bdd4f9cd65317b856dc2c518ec4e936
Volodymyr <degtyarevvladimirr@gmail.com>
2024-06-16 12:16:44 +0300

    fix: update avatar

--- src/utilities/handleFavorite.js
+++ src/utilities/handleFavorite.js
@@ -1,9 +1,13 @@
 import { toast } from "react-toastify";
 
+const customId = "toastId";
+
 const handleFavorite = async (funcName, recipeID, action, setFunc) => {
   try {
     await funcName(recipeID).unwrap();
-    toast.success(`Recipe ${action === "add" ? "added to" : "deleted from"} favorites!`);
+    toast.success(`Recipe ${action === "add" ? "added to" : "deleted from"} favorites!`, {
+      toastId: customId,
+    });
     action === "add" ? setFunc(true) : setFunc(false);
   } catch (error) {
     console.error("Failed to add recipe to favorites: ", error);
