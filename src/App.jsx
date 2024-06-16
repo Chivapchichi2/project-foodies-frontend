1924fb613b369e7d2a6d08b6d6f651e851aef19f
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-06-16 11:47:17 +0200

    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { useGetFavoriteRecipesQuery } from "./store/services/recipeService";
 import { setFavoriteRecipes } from "./store/features/favoriteRecipesSlice";
 import { selectToken } from "./store/features/authSlice.js";
 import { Recipes } from "src/components/Recipes/Recipes.jsx";
+import PageNotFound from "./components/ErrorComponents/PageNotFound/PageNotFound.jsx";
 
 const Home = lazy(() => import("src/pages/Home/Home"));
 const Recipe = lazy(() => import("src/pages/Recipe/Recipe"));
@@ -42,6 +43,7 @@ export const App = () => {
           <Route path="/recipe/add" element={<PrivateRoute component={AddRecipe} />} />
           <Route path="/recipe/:id" element={<Recipe />} />
           <Route path="/user/:id" element={<PrivateRoute component={User} />} />
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
